3a6c63d712a7d081343bdbcccfeda36ef68daed3
mawekk <mariamya243@gmail.com>
2022-10-16 02:32:45 +0300

    Add transform

---
+++ Homework1/ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Task2;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string example = "ABACABA";
+            Console.WriteLine("Before: " + example);
+            var transformed = BurrowsWheelerTransformation.Transform(example);
+            Console.WriteLine("After BWT: " + transformed.Item1 + ", " + transformed.Item2);
+        }
+    }
+}
---
+++ Homework1/Task2/BWT.cs
@@ -0,0 +1,42 @@
+﻿namespace Task2;
+
+public static class Bwt
+{
+    public static (string result, int endPosition) Transform(string text)
+    {
+        var suffixArray = ConstructSuffixArray(text).ToArray();
+        var result = "";
+        var endPosition = 0;
+
+        for (var i = 0; i < text.Length; i++)
+        {
+            if (suffixArray[i] == 0)
+            {
+                result += text.Last();
+                endPosition = i;
+            }
+            else
+            {
+                result += text[suffixArray[i] - 1];
+            }
+            
+            Console.WriteLine(suffixArray[i]);
+        }
+        return (result, endPosition);
+    }
+
+    private static IEnumerable<int> ConstructSuffixArray(string text)
+    {
+        var suffixArray = new List<int>();
+
+        for (var i = 0; i < text.Length; i++)
+        {
+            suffixArray.Add(i);
+        }
+
+        suffixArray.Sort(((i, j) =>
+            string.Compare(text[i..], text[j..], StringComparison.Ordinal)));
+
+        return suffixArray;
+    }
+}
