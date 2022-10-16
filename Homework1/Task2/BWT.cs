c5fa30688535a9e41b202be1a359f65af7da36d9
mawekk <mariamya243@gmail.com>
2022-10-16 05:01:19 +0300

    Remove unnecessary line (cringe)

--- Homework1/Task2/BWT.cs
+++ Homework1/Task2/BWT.cs
@@ -1,48 +1,48 @@
-﻿namespace Task2;
+﻿using System.Text;
+
+namespace Task2;
 
 public static class Bwt
 {
     public static (string result, int endPosition) Transform(string text)
     {
         var suffixArray = ConstructSuffixArray(text).ToArray();
-        var result = "";
+        var result = new StringBuilder(text.Length);
         var endPosition = 0;
 
         for (var i = 0; i < text.Length; i++)
         {
             if (suffixArray[i] == 0)
             {
-                result += text.Last();
+                result.Append(text.Last());
                 endPosition = i;
             }
             else
             {
-                result += text[suffixArray[i] - 1];
+                result.Append(text[suffixArray[i] - 1]);
             }
-            
-            Console.WriteLine(suffixArray);
         }
-        return (result, endPosition);
+        return (result.ToString(), endPosition);
     }
     
     public static string Retransform(string text, int endPosition)
     {
-        var result = "";
+        var result = new StringBuilder(text.Length);
         var transformationArray =
             text.Select((symbol, index) => (symbol, index)).OrderBy(pair => pair.symbol).ToArray();
 
         foreach (var _ in text)
         {
-            result += text[transformationArray[endPosition].index];
+            result.Append(text[transformationArray[endPosition].index]);
             endPosition = transformationArray[endPosition].index;
         }
         
-        return result;
+        return result.ToString();
     }
     
     private static IEnumerable<int> ConstructSuffixArray(string text)
     {
-        var suffixArray = new List<int>();
+        var suffixArray = new List<int>(text.Length);
 
         for (var i = 0; i < text.Length; i++)
         {
