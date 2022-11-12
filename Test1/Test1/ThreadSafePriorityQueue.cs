47c5d44e6340caa383c53670e1ca916294fc62c8
mawekk <mariamya243@gmail.com>
2022-11-12 23:54:05 +0300

    Add solution

---
+++ Test1/Test1/ThreadSafePriorityQueue.cs
@@ -0,0 +1,34 @@
+﻿namespace Test1;
+
+public class ThreadSafePriorityQueue<P, V > where P : IComparable<P>
+{
+    private readonly SortedList<P, V> list = new();
+    private int count = 0;
+    private readonly object listLock = new();
+
+    public void Enqueue(P priority, V value)
+    {
+        lock (listLock)
+        {
+            list.Add(priority, value);
+            count++;
+            Monitor.PulseAll(listLock);
+        }
+    }
+
+    public V Dequeue()
+    {
+        lock (listLock)
+        {
+            if (list.Count == 0) Monitor.Wait(listLock);
+
+            var element = list.Values.Last();
+            list.RemoveAt(list.Count - 1);
+            count--;
+
+            return element;
+        }
+    }
+
+    public int Size() => count;
+}
