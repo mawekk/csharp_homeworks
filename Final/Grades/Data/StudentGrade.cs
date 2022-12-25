c633ea70dfa36e7eedc5cd0fe00e536329d8b6ef
mawekk <mariamya243@gmail.com>
2022-12-25 22:06:20 +0300

    Add solution

---
+++ Final/Grades/Data/StudentGrade.cs
@@ -0,0 +1,9 @@
+﻿namespace Grades.Data;
+
+public class StudentGrade
+{
+    public int StudentGradeID { get; set; }
+    public string Name { get; set; } = "";
+    public string Subject { get; set; } = "";
+    public int Grade { get; set; }
+}
---
+++ Final/Grades/Pages/AddGrade.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Grades.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grades.Pages;
+
+[BindProperties]
+public class AddGrade : PageModel
+{
+    private readonly StudentGradeDbContext _context;
+    public AddGrade(StudentGradeDbContext context)
+        => _context = context;
+    public StudentGrade StudentGrade { get; set; } = new();
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        _context.StudentGrades.Add(StudentGrade);
+
+        await _context.SaveChangesAsync();
+        return RedirectToPage("./Index");
+    }
+}
