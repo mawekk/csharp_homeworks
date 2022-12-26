1a2f94983d197ec908cfdab014f3bd7d70b04698
mawekk <mariamya243@gmail.com>
2022-12-26 23:26:52 +0300

    Add solution 2.0

--- Final/Grades/Data/StudentGrade.cs
+++ Final/Grades/Data/StudentGrade.cs
@@ -1,9 +1,18 @@
-﻿namespace Grades.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grades.Data;
 
 public class StudentGrade
 {
     public int StudentGradeID { get; set; }
+    
+    [Required(ErrorMessage = "Кому выставить оценочку?")]
     public string Name { get; set; } = "";
+    
+    [Required(ErrorMessage = "А за что оценочку выставить?")]
     public string Subject { get; set; } = "";
+    
+    [Required(ErrorMessage = "Поставьте оценочку от 0 до 10")]
+    [Range(0, 10, ErrorMessage = "Поставьте оценочку от 0 до 10")]
     public int Grade { get; set; }
 }
--- Final/Grades/Pages/AddGrade.cshtml.cs
+++ Final/Grades/Pages/AddGrade.cshtml.cs
@@ -1,6 +1,5 @@
-﻿using Grades.Data;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Threading.Tasks;
+using Grades.Data;
 
 namespace Grades.Pages;
 
@@ -8,15 +7,22 @@ namespace Grades.Pages;
 public class AddGrade : PageModel
 {
     private readonly StudentGradeDbContext _context;
+
     public AddGrade(StudentGradeDbContext context)
         => _context = context;
+
     public StudentGrade StudentGrade { get; set; } = new();
 
     public async Task<IActionResult> OnPostAsync()
     {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
         _context.StudentGrades.Add(StudentGrade);
 
         await _context.SaveChangesAsync();
-        return RedirectToPage("./Index");
+        return RedirectToPage("Index");
     }
 }
